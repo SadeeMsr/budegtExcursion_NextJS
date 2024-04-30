511dc38952808fc34bbfa82fe6ce747eea1512ab
Md Sadee Rohan <srrohan333@gmail.com>
2024-04-30 11:21:42 +0600

    site-map package added

--- next-sitemap.js
+++
@@ -1,5 +0,0 @@
-
-module.exports = {
-    siteUrl,
-    generateRobotsTxt: true, // (optional)
-  }
---
+++ sitemap.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  siteUrl: 'https://budegt-excursion-next-rjbbwsafp-sadeemsrs-projects.vercel.app/',
+  generateRobotsTxt: true, // (optional)
+  // ...other options
+}
